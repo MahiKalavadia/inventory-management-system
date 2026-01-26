75a38a901d4c5ca1ff150afd5fa47b33aeac6dff
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-01-26 10:43:22 +0530

    made secret key a secret

--- electronics_inventory/settings.py
+++ electronics_inventory/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tj@^7_i0wt6eyei@1@79-@e!41bu$yvo50wjc)ld2fc6=vb#0b'
+SECRET_KEY = os.environ.get('INVENT_KEY', 'dev-fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
