d1c418aaa3e359544c81ae31fdc1bbddab360fac
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-02-05 12:42:38 +0530

    Added profit , purchase_price and margin field into Product model

--- inventory/models.py
+++ inventory/models.py
@@ -15,7 +15,13 @@ class Product(models.Model):
     sku = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=200)
     brand = models.CharField(max_length=100)
+    purchase_price = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    profit = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
+    margin_percent = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True)
     quantity = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     supplier = models.ForeignKey(
@@ -30,6 +36,25 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} {self.sku}'
+
+    def save(self, *args, **kwargs):
+        if self.purchase_price and self.price:
+            self.profit = self.price - self.purchase_price
+            self.margin_percent = (self.profit / self.purchase_price) * 100
+        super().save(*args, **kwargs)
+
+    @property
+    def profit_per_unit(self):
+        if self.price and self.purchase_price:
+            return self.price - self.purchase_price
+        return 0
+
+
+    @property
+    def stock_value(self):
+       if self.purchase_price and self.quantity:
+          return self.purchase_price * self.quantity
+       return 0
 
 
 class StockLog(models.Model):
