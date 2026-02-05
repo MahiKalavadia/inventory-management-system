d3f411d10eb7909d7dabafb577f2f97123146742
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-02-05 12:47:05 +0530

    Added the mark all as read notification

--- notifications/urls.py
+++ notifications/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('mark-read/<int:pk>/', views.mark_as_read, name='mark_as_read'),
     path('delete/<int:pk>/', views.delete_notfification,
          name='delete_notification'),
+    path("mark-all-read/", views.mark_all_notifications_read,
+         name="mark_all_notifications_read"),
 ]
