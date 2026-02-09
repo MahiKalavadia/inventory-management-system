d051d993dd3b2fdfcf646490ff08e7a255d8c324
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-02-09 10:33:00 +0530

    Added the url for toggle supplier status view

--- suppliers/urls.py
+++ suppliers/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import supplier_list, supplier_dashboard, add_supplier, update_supplier, delete_supplier, active_supplier, inactive_supplier
+from .views import supplier_list, supplier_dashboard, add_supplier, update_supplier, delete_supplier, active_supplier, inactive_supplier, toggle_supplier_status
 
 urlpatterns = [
     path('suppliers/dashboard/', supplier_dashboard, name="supplier_dashboard"),
     path('suppliers/list/', supplier_list, name='supplier_list'),
     path('suppliers/active', active_supplier, name="active_supplier"),
     path('suppliers/inactive', inactive_supplier, name="inactive_supplier"),
+    path('supplier/toggle_supplier_status/<int:pk>/',
+         toggle_supplier_status, name="toggle_supplier_status"),
     path('suppliers/add/', add_supplier, name='add_supplier'),
     path('suppliers/update/<int:pk>/', update_supplier, name='update_supplier'),
     path('suppliers/delete/<int:pk>/', delete_supplier, name='delete_supplier'),
