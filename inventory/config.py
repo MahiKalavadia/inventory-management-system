7c159fb61865a5ea27bdbb201976379b3bfc1ae3
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-02-18 14:08:40 +0530

    Changed the file connected it to Settings Model

--- inventory/config.py
+++ inventory/config.py
@@ -1,1 +1,10 @@
-LOW_STOCK_THRESHOLD = 5
+DEFAULT_LOW_STOCK_THRESHOLD = 5
+
+
+def get_low_stock_threshold():
+    try:
+        from settings_app.models import SystemSettings
+        return SystemSettings.load().low_stock_threshold
+    except Exception:
+        return DEFAULT_LOW_STOCK_THRESHOLD
+
