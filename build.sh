ccbb2860094e49f2c8d132d5e3c5dcc40c817b88
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-03-03 17:23:10 +0530

    Removed loaddata command from build

--- build.sh
+++ build.sh
@@ -3,11 +3,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
-
-python manage.py loaddata categories.json
-python manage.py loaddata suppliers.json
-python manage.py loaddata groups.json
-python manage.py loaddata users.json
-python manage.py loaddata inventory.json
-python manage.py loaddata orders.json.gz
-python manage.py loaddata purchases.json
