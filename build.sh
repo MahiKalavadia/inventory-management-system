fbaa54d59db65bddec92763f49e9789f1696d8b0
MahiKalavadia <mahikalavadiya172006@gmail.com>
2026-03-03 10:19:58 +0530

    fix build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --no-input
 
-gunzip -c data.json.gz > data.json
-python manage.py loaddata ./data.json
-python manage.py loaddata ./users.json
+python manage.py loaddata data.json.gz
+python manage.py loaddata users.json
